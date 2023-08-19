0d8ce4760aef2c1034d7ebcf20e8f0b102f6256e
KarolDiniz <dinizkaroline@gmail.com>
2023-08-18 22:51:48 -0300

    Creating project

---
+++ AnimalAdoption/src/test/java/br/edu/ifpb/dac/karoline/animaladoption/AnimalAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.karoline.animaladoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
