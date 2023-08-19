8a1c8014e2b8cf7f15de4ea2d0cd04b2fce55bd8
KarolDiniz <dinizkaroline@gmail.com>
2023-08-18 22:54:13 -0300

    Creating controller

---
+++ AnimalAdoption/src/main/java/br/edu/ifpb/dac/karoline/animaladoption/business/controller/UserController.java
@@ -0,0 +1,41 @@
+package br.edu.ifpb.dac.karoline.animaladoption.business.controller;
+
+import br.edu.ifpb.dac.karoline.animaladoption.business.service.UserService;
+import br.edu.ifpb.dac.karoline.animaladoption.model.entities.Animal;
+import br.edu.ifpb.dac.karoline.animaladoption.model.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    public User createUser(User user) {
+        return userService.createUser(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        return userService.updateUser(id, user);
+    }
+
+    public User findByUsername(String username) {
+        return userService.findByUsername(username);
+    }
+
+    public List<Animal> getUserAnimals(Long userId) {
+        return userService.getAnimalsByUserId(userId);
+    }
+    public void deleteUser(Long id) {
+        userService.deleteUser(id);
+    }
+
+}
+
